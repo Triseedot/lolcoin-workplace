ada91531c7faf875ff87ed8ab1f22a9c3dcb6a29
Valerii <wdgsyq24dh@gmail.com>
2022-08-16 23:08:20 +0200

    Pre-alpha 0.1

--- main.py
+++ main.py
@@ -3,27 +3,28 @@ from aiogram import Bot, types
 from aiogram.types import Message
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
-from aiogram.dispatcher.webhook import SendMessage
 from aiogram.utils.executor import start_webhook
 import logging
-# from config import *
 
+# bot initialization
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 logging.basicConfig(level=logging.INFO)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
+# webhook settings
 APP_NAME = os.getenv('APP_NAME')
 WEBHOOK_HOST = f'https://{APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.getenv("PORT", default=8000))
 
 
+# main part with all bot commands
 async def on_startup(dispatcher):
     await bot.delete_webhook()
     await bot.set_webhook(WEBHOOK_URL)
@@ -39,7 +40,6 @@ async def on_shutdown(dispatcher):
 
 @dp.message_handler(commands=['start'])
 async def start(message: Message):
-    logging.warning('Start command!')
     await message.answer('Oh, hello there!')
 
 
@@ -48,6 +48,7 @@ async def echo(message: Message):
     await message.answer(message.text)
 
 
+# bot start
 if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
